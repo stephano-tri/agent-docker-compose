bb2913a09e55fc9e67bfd71248c58b6e93a82404
stephano-tri <mtriumph23@gmail.com>
2023-11-29 16:06:26 +0900

    ADD: add create_centos.sh for auto creating centos

---
+++ create_centos.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+destination_file="docker-compose.yml"
+
+# Appending text to the destination file
+last_number=$(docker ps --format '{{.Names}}' | sort -V | tail -n 1 | cut -c7-)
+result=$((last_number + 1))
+
+offsetContainerPort=8095
+offsetSSHInstancePort=10022
+
+manipulatedContainerPort=$((last_number + offsetContainerPort))
+manipulatedSSHInstancePort=$((last_number + offsetSSHInstancePort))
+
+centos_docker="
+  centos$result:
+      networks:
+        - network
+      hostname: centos$result
+      container_name: centos$result
+      healthcheck:
+        test: [ "CMD-SHELL", "ps-aux" ]
+        interval: 30s
+        timeout: 10s
+        retries: 3
+        start_period: 5s
+      restart: always
+      ports:
+        - "$manipulatedContainerPort:$manipulatedContainerPort"
+        - "$manipulatedSSHInstancePort:22"
+      deploy:
+        resources:
+          limits:
+            cpus: \"1\"
+            memory: \"3g\"
+      memswap_limit: \"5g\"
+      build:
+        context: .
+        dockerfile: ./centos$result/Dockerfile
+      stdin_open: true
+      tty: true
+      volumes:
+        - ./centos$result/common:/common
+        - ./disk$result:/sys/fs/cgroup"
+
+echo -e "$centos_docker" >> $destination_file
+
+cp -r "centos1" "centos$result"
+cp -r "disk1" "disk$result"
+
+docker-compose up -d
+
+echo "New Container Configuration Added and Start"
